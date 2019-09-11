9579aef72d1e54c000089720ae937672fa5e0d4d
zzj <1907455081@qq.com>
2019-09-11 09:02:54 +0800

    fix

---
+++ src/main/java/com/example/git1/Git1Application.java
@@ -0,0 +1,13 @@
+package com.example.git1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git1Application {
+
+	public static void main(String[] args) {
+		System.out.println("bcjbfdk");
+	}
+
+}
