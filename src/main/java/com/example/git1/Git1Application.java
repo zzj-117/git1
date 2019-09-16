25160399baa928921a4bf9170ecd90e2e920fe61
zzj <1907455081@qq.com>
2019-09-16 08:50:03 +0800

    new file

--- src/main/java/com/example/git1/Git1Application.java
+++ src/main/java/com/example/git1/Git1Application.java
@@ -1,13 +1,24 @@
 package com.example.git1;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@RestController
 public class Git1Application {
 
 	public static void main(String[] args) {
 		System.out.println("bhxdj");
 	}
 
+
+	@RequestMapping("/hello/{name}")
+	public String sayHello(@PathVariable("name") String name) {
+		return "hello, " + name +", this is spring boot demo";
+	}
 }
